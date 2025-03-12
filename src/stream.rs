1f29e3d70b3beb66646c3b221726cbecac6470ed
kostasgr100 <97601678+kostasgr100@users.noreply.github.com>
2025-03-12 21:30:52 +0200

    Update stream.rs

--- src/stream.rs
+++ src/stream.rs
@@ -1,12 +1,11 @@
 use crate::buffer::{SyncReadAdaptor, SyncWriteAdaptor};
 
-use rustls::{ClientConfig, ClientConnection, ConnectionCommon, SideData, pki_types::ServerName};
+use rustls::{ConnectionCommon, SideData};
 use std::{
     io::{self, Read, Write},
     ops::{Deref, DerefMut},
-    sync::Arc,
 };
-use tokio_uring::{net::TcpStream, buf::{IoBuf, IoBufMut}, BufResult};
+use tokio_uring::{net::TcpStream, BufResult};
 
 pub struct TlsStream<C> {
     pub(crate) io: TcpStream,
@@ -28,25 +27,12 @@ where
         }
     }
 
-    pub fn new_client(
-        io: TcpStream,
-        config: Arc<ClientConfig>,
-        domain: ServerName<'static>,
-    ) -> io::Result<TlsStream<ClientConnection>> {
-        let session = ClientConnection::new(config, domain)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        Ok(TlsStream {
-            io,
-            session,
-            rbuffer: SyncReadAdaptor::default(),
-            wbuffer: SyncWriteAdaptor::default(),
-        })
-    }
-
     async fn read_io(&mut self) -> io::Result<usize> {
         let n = loop {
             match self.session.read_tls(&mut self.rbuffer) {
-                Ok(n) => break n,
+                Ok(n) => {
+                    break n;
+                }
                 Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
                     self.rbuffer.do_io(&mut self.io).await?;
                     continue;
@@ -57,7 +43,16 @@ where
 
         let state = match self.session.process_new_packets() {
             Ok(state) => state,
-            Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err)),
+            Err(err) => {
+                // When to write_io? If we do this in read call, the UnsafeWrite may crash
+                // when we impl split in an UnsafeCell way.
+                // Here we choose not to do write when read.
+                // User should manually shutdown it on error.
+                // if !splitted {
+                //     let _ = self.write_io().await;
+                // }
+                return Err(io::Error::new(io::ErrorKind::InvalidData, err));
+            }
         };
 
         if state.peer_has_closed() && self.session.is_handshaking() {
@@ -73,7 +68,9 @@ where
     async fn write_io(&mut self) -> io::Result<usize> {
         let n = loop {
             match self.session.write_tls(&mut self.wbuffer) {
-                Ok(n) => break n,
+                Ok(n) => {
+                    break n;
+                }
                 Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
                     self.wbuffer.do_io(&mut self.io).await?;
                     continue;
@@ -83,10 +80,11 @@ where
         };
 
         self.wbuffer.do_io(&mut self.io).await?;
+
         Ok(n)
     }
 
-    pub async fn handshake(&mut self) -> io::Result<(usize, usize)> {
+    pub(crate) async fn handshake(&mut self) -> io::Result<(usize, usize)> {
         let mut wrlen = 0;
         let mut rdlen = 0;
         let mut eof = false;
@@ -106,16 +104,17 @@ where
 
             match (eof, self.session.is_handshaking()) {
                 (true, true) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::UnexpectedEof,
-                        "tls handshake eof",
-                    ));
+                    let err = io::Error::new(io::ErrorKind::UnexpectedEof, "tls handshake eof");
+                    return Err(err);
                 }
                 (false, true) => (),
-                (_, false) => break,
+                (_, false) => {
+                    break;
+                }
             };
         }
 
+        // flush buffer
         while self.session.wants_write() {
             wrlen += self.write_io().await?;
         }
@@ -123,19 +122,29 @@ where
         Ok((rdlen, wrlen))
     }
 
-    pub async fn read<B: IoBufMut>(&mut self, mut buf: B) -> BufResult<usize, B> {
-        let slice = unsafe { std::slice::from_raw_parts_mut(buf.stable_mut_ptr(), buf.bytes_total()) };
+    pub async fn read<B: tokio_uring::buf::IoBufMut>(&mut self, mut buf: B) -> BufResult<usize, B> {
+        // Safety: bytes_total property promises the capacity of the buffer, such that we won't overrun.
+        let slice =
+            unsafe { std::slice::from_raw_parts_mut(buf.stable_mut_ptr(), buf.bytes_total()) };
 
         loop {
+            // read from rustls to buffer
             match self.session.reader().read(slice) {
                 Ok(n) => {
+                    // Safety: we already know from the reader that we have read n bytes, so the n bytes must
+                    // be stored in the buffer.
                     unsafe { buf.set_init(n) };
+
                     return (Ok(n), buf);
                 }
+                // we need more data, read something.
                 Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => (),
-                Err(e) => return (Err(e), buf),
+                Err(e) => {
+                    return (Err(e), buf);
+                }
             }
 
+            // now we need data, read something into rustls
             match self.read_io().await {
                 Ok(0) => {
                     return (
@@ -147,12 +156,14 @@ where
                     );
                 }
                 Ok(_) => (),
-                Err(e) => return (Err(e), buf),
-            }
+                Err(e) => {
+                    return (Err(e), buf);
+                }
+            };
         }
     }
 
-    pub async fn write<B: IoBuf>(&mut self, buf: B) -> BufResult<usize, B> {
+    pub async fn write<B: tokio_uring::buf::IoBuf>(&mut self, buf: B) -> BufResult<usize, B> {
         let slice = unsafe { std::slice::from_raw_parts(buf.stable_ptr(), buf.bytes_init()) };
 
         let size = match self.session.writer().write(slice) {
@@ -166,16 +177,18 @@ where
 
         while self.session.wants_write() {
             match self.write_io().await {
-                Ok(0) => break,
+                Ok(0) => {
+                    break;
+                }
                 Ok(_) => (),
                 Err(e) => return (Err(e), buf),
             }
         }
 
-        (Ok(size), buf)
+        return (Ok(size), buf);
     }
 
-    pub async fn write_all<B: IoBuf>(&mut self, buf: B) -> BufResult<(), B> {
+    pub async fn write_all<B: tokio_uring::buf::IoBuf>(&mut self, buf: B) -> BufResult<(), B> {
         let slice = unsafe { std::slice::from_raw_parts(buf.stable_ptr(), buf.bytes_init()) };
 
         if let Err(e) = self.session.writer().write_all(slice) {
@@ -188,12 +201,14 @@ where
 
         while self.session.wants_write() {
             match self.write_io().await {
-                Ok(0) => break,
+                Ok(0) => {
+                    break;
+                }
                 Ok(_) => (),
                 Err(e) => return (Err(e), buf),
             }
         }
 
-        (Ok(()), buf)
+        return (Ok(()), buf);
     }
 }
